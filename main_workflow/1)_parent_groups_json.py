dc7568d8a002d452afce8d1006a244d2844aefb6
Woods26 <forest.bremer@gmail.com>
2016-11-04 17:27:14 -1000

    code clean up

--- main_workflow/1)_parent_groups_json.py
+++ main_workflow/1)_parent_groups_json.py
@@ -53,7 +53,8 @@ for sp in gff:
             ortho = acc_ortho_dict[sp][acc]
             if ortho not in parent_groups:
                 parent_groups[ortho] = {}
-            parents = [parent for parent in gff[sp].parents(cds) if parent.featuretype in ["mRNA", "prediction"]]
+            parents = [parent for parent in gff[sp].parents(cds) if
+                       parent.featuretype in ["mRNA", "prediction"]]
             if len(parents) is not 1:
                 print("error in cds: {}\nparents: {}".format(cds, parents))
             parent_groups[ortho][sp] = parents[0].id
--- main_workflow/6)_pad_to_13spp_and_trim_to_primer_product.py
+++ main_workflow/6)_pad_to_13spp_and_trim_to_primer_product.py
@@ -1,22 +1,18 @@
-
 # coding: utf-8
 
 # In[ ]:
 
-import os
-import gffutils
-from Bio.Seq import Seq
-from Bio.SeqRecord import SeqRecord
-from Bio.Alphabet import IUPAC
 import json
-from pyfaidx import Fasta
-from Bio import SeqIO
-import re
+import os
 
+from Bio import SeqIO
+from Bio.Alphabet import IUPAC
+from Bio.SeqRecord import SeqRecord
 
 # In[ ]:
 
-full_species_list = ['Bjar', 'Aobl', 'Bmin', 'Asus', 'Btry', 'Afra', 'Blat', 'Bzon', 'Bcor', 'Ccap', 'Bcur', 'Bole', 'Bdor']
+full_species_list = ['Bjar', 'Aobl', 'Bmin', 'Asus', 'Btry', 'Afra', 'Blat', 'Bzon', 'Bcor',
+                     'Ccap', 'Bcur', 'Bole', 'Bdor']
 species_list = ["Bcur", "Bdor", "Bole", "Ccap"]
 transvestigated_species_set = {'Bcor', 'Blat', 'Bzon', 'Afra', 'Bmin', 'Bjar', 'Aobl'}
 
@@ -26,29 +22,26 @@ aligned_fasta_path = "../output/orthoCds/"
 p3_out_path = "../intermediate/primer_design/"
 json_path = "../intermediate/json/"
 
-
 # In[ ]:
 
 with open(json_path + "alternate_sp.json", 'r') as f:
     alternate_sp = json.load(f)
 
-
 # In[ ]:
 
 # create handles for all .fasta files in fasta directory
-fasta_fn = {name.split('.13spp.fasta')[0]: aligned_fasta_path + name for name in os.listdir(aligned_fasta_path) if
-         ((".13spp.fasta" in name) and (".13spp.fasta.fai" not in name))}
-
+fasta_fn = {name.split('.13spp.fasta')[0]: aligned_fasta_path + name for name in
+            os.listdir(aligned_fasta_path) if
+            ((".13spp.fasta" in name) and (".13spp.fasta.fai" not in name))}
 
 # In[ ]:
 
 # read and parse fasta files for each species
 fasta = {}
 for ortho in fasta_fn.keys():
-    fasta[ortho] = {seq_record.id : seq_record 
-                                      for seq_record in SeqIO.parse(fasta_fn[ortho],
-                                                                    "fasta", alphabet=IUPAC.ambiguous_dna)}
-
+    fasta[ortho] = {seq_record.id: seq_record
+                    for seq_record in SeqIO.parse(fasta_fn[ortho],
+                                                  "fasta", alphabet=IUPAC.ambiguous_dna)}
 
 # In[ ]:
 
@@ -58,29 +51,26 @@ for p3_out_fn in os.listdir(p3_out_path):
     with open(p3_out_path + p3_out_fn, 'r') as f:
         lines = f.readlines()
         lines = [line.strip().split('=') for line in lines]
-        lines = {key:value for key,value in lines if key is not ''}
+        lines = {key: value for key, value in lines if key is not ''}
         if lines['PRIMER_PAIR_NUM_RETURNED'] is not '0':
             test = lines
-            left,l_len = lines['PRIMER_LEFT_0'].split(',')
-            right,r_len = lines['PRIMER_RIGHT_0'].split(',')
+            left, l_len = lines['PRIMER_LEFT_0'].split(',')
+            right, r_len = lines['PRIMER_RIGHT_0'].split(',')
             start = int(left) + int(l_len)
             end = int(right) - int(r_len) + 1
-            primer[ortho] = (start,end)
-
-
-# In[ ]:
-
-trimmed_fasta = {ortho:{sp:fasta[ortho][sp][start:end] for sp in fasta[ortho]}}
-
+            primer[ortho] = (start, end)
 
 # In[ ]:
 
-from copy import deepcopy
+# trimmed_fasta = {ortho: {sp: fasta[ortho][sp][start:end] for sp in fasta[ortho]}}
+
+# In[ ]:
+
 padded_fasta = {}
 trimmed_fasta = {}
 for ortho in fasta.keys():
     if ortho in primer.keys():
-        start,end = primer[ortho]
+        start, end = primer[ortho]
     else:
         continue
     padding = {}
@@ -93,34 +83,31 @@ for ortho in fasta.keys():
                     des = "PADDING " + des
                     padding[sp] = SeqRecord(seq, id=sp, description=des)
                     break
-    trimmed_fasta[ortho] = {sp:fasta[ortho][sp][start:end] for sp in fasta[ortho]}
+    trimmed_fasta[ortho] = {sp: fasta[ortho][sp][start:end] for sp in fasta[ortho]}
     padded_fasta[ortho] = padding
     padded_fasta[ortho].update(trimmed_fasta[ortho])
 
-
 # In[ ]:
 
-sp_order = { 'Bcur':1,
-             'Bdor':2,
-             'Bole':3,
-             'Ccap':4,
-             'Bcor':5,
-             'Blat':6,
-             'Bzon':7,
-             'Afra':8,
-             'Bmin':9,
-             'Bjar':10,
-             'Aobl':11,
-             'Asus':12,
-             'Btry':13}
-
+sp_order = {'Bcur': 1,
+            'Bdor': 2,
+            'Bole': 3,
+            'Ccap': 4,
+            'Bcor': 5,
+            'Blat': 6,
+            'Bzon': 7,
+            'Afra': 8,
+            'Bmin': 9,
+            'Bjar': 10,
+            'Aobl': 11,
+            'Asus': 12,
+            'Btry': 13}
 
 # In[ ]:
 
 # output fasta to pre_padding_species.json
 with open(json_path + "pre_padding_species.json", 'w') as f:
-    json.dump({ortho:[sp for sp in trimmed_fasta[ortho]] for ortho in trimmed_fasta}, f)
-
+    json.dump({ortho: [sp for sp in trimmed_fasta[ortho]] for ortho in trimmed_fasta}, f)
 
 # In[ ]:
 
@@ -129,11 +116,9 @@ for ortho in trimmed_fasta.keys():
         for seqReq in sorted(trimmed_fasta[ortho].values(), key=lambda x: sp_order[x.id]):
             f.write(seqReq.format("fasta"))
 
-
 # In[ ]:
 
 for ortho in padded_fasta.keys():
     with open(output_padded_path + ortho + ".13spp.fasta", "w") as f:
         for seqReq in sorted(padded_fasta[ortho].values(), key=lambda x: sp_order[x.id]):
             f.write(seqReq.format("fasta"))
-
