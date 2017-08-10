d3c5018fdddb9a93818d8c3ebb02d8f6634e0a16
Woods26 <forest.bremer@gmail.com>
2017-08-09 17:13:09 -1000

    typo in comments

--- osx_create_anaconda_environments.sh
+++ osx_create_anaconda_environments.sh
@@ -6,7 +6,7 @@ conda env create -f environment.yml
 conda env create -f mafft_env.yml
 conda env create -f tapir_env.yml
 
-# the tapir environment still needs hiphy2
+# the tapir environment still needs hyphy2
 source activate tapir
 
 wget http://s3.faircloth-lab.org/packages/hyphy2.osx.gz
