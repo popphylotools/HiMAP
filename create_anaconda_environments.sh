df540c3e3478dd657381d1ea0bb71275daef043a
Woods26 <forest.bremer@gmail.com>
2017-08-09 16:54:00 -1000

    improved install scripts

--- create_anaconda_environments.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-conda env create -f environment.yml
-conda env create -f mafft_env.yml
-conda env create -f tapir_env.yml
-
-# before running python scripts, run `source activate orthoExon`
---
+++ osx_create_anaconda_environments.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# before running python scripts, run `source activate orthoExon`
+
+conda env create -f environment.yml
+conda env create -f mafft_env.yml
+conda env create -f tapir_env.yml
+
+# the tapir environment still needs hiphy2
+source activate tapir
+
+wget http://s3.faircloth-lab.org/packages/hyphy2.osx.gz
+gunzip hyphy2.*.gz
+chmod 0700 hyphy2.*
+mv hyphy2.* $(dirname $(which python))/hyphy2
+
+rm hyphy2*
+
+source deactivate
